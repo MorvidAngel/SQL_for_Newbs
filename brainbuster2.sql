70dccb4ca1d7f7ab690c9fffa91e78d61df2fe86
morvid <y7q9c3m4f2h6r1@hotmail.com>
2022-12-24 22:46:28 -0500

    sql

---
+++ brainbuster2.sql
@@ -0,0 +1,13 @@
+-- which rating is most prevalent in each store?
+
+
+-- which rating do we have the most films in?
+-- select count(rating), rating
+-- from film
+-- group by rating
+-- ;
+
+
+-- new 2022
+-- Which rating is most prevalent in each price 
+-- (use only 1 query)?
